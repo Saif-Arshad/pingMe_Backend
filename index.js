56dd3daa35c5e58def342695834b2a54beadf70a
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-08-22 12:18:12 +0500

    feat : add dummy API routes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+const express = require('express');
+
+const port = '3000'
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    console.log("🚀 ~ app.get ~ req:", req)
+    res.send("Hi i am Saif Ur Rehman")
+})
+app.get('/login', (req, res) => {
+    console.log("🚀 ~ app.get ~ req:", req)
+    res.send("You are at login page")
+})
+app.post('/me', (req, res) => {
+    const data = req.body
+    console.log("🚀 ~ app.post ~ data:", data)
+    console.log("🚀 ~ app.get ~ req:", req)
+    res.send(`Your name is ${data.name} and your email is ${data.email}`)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
