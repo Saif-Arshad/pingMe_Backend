f1a882ab1ce1420e26d186c10fe051cde35c5482
saif ur rehman <saifarshad3344@gmail.com>
2024-10-14 16:17:05 +0500

    fix : JWT middleware

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -6,7 +6,7 @@ const verifyUserToken = async (req, res, next) => {
         let token = req.header("Authorization");
 
         if (!token) {
-            return res.error({ status: 403, message: "Access Denied" });
+            return res.status(403).json({ status: 403, message: "Access Denied" });
         }
 
         if (token.startsWith("Bearer ")) {
@@ -19,13 +19,13 @@ const verifyUserToken = async (req, res, next) => {
 
         // Check if the token has expired
         if (decoded.exp && Date.now() >= decoded.exp * 1000) {
-            return res.error({ status: 401, message: "Expired token" });
+            return res.status(401).json({ status: 401, message: "Expired token" });
         }
 
         // Check if the token exists in the database
         const tokenInstance = await UserToken.findOne({ token: token });
         if (!tokenInstance) {
-            return res.error({ status: 401, message: "Session expired" });
+            return res.status(401).json({ status: 401, message: "Session expired" });
         }
 
         // Pass the token to the next middleware
@@ -33,14 +33,14 @@ const verifyUserToken = async (req, res, next) => {
         next();
     } catch (error) {
         if (error.name === "JsonWebTokenError") {
-            return res.error({ status: 401, message: "Invalid token" });
+            return res.status(401).json({ status: 401, message: "Invalid token" });
         }
 
         if (error.name === "TokenExpiredError") {
-            return res.error({ status: 401, message: "Expired token" });
+            return res.status(401).json({ status: 401, message: "Expired token" });
         }
 
-        return res.erorr({ status: 500, message: "Please provide token" });
+        return res.status(500).json({ status: 500, message: "Please provide token" });
     }
 };
 
